86c64d18342072f1469342411b70ecc63adb6a2e
Cheng Xun Chua <chengxunchua@Chengs-MacBook-Air.local>
2016-02-24 23:51:05 +0800

    jquery version

--- script2.js
+++ script2.js
@@ -3,52 +3,40 @@ console.clear();
 var player1sTurn = true;
 var isGameOver = false;
 var turns = 0;
-var elements = document.getElementsByClassName("square");
 
 function ready(){
   console.log('ready');
-  console.log(elements.length + " elements found");
-  document.getElementById("button1").addEventListener('click',function(event){
-    resetGame();
-  },false);
+  console.log($('.square').length + " elements found");
+  $('#button1').on('click',resetGame);
+  $('.square').on('click',playTurn);
 
-  for ( var i = 0; i < elements.length; i++ ){
-      addClickListener(elements[i], i);
-  }
-}
-
-function addClickListener(element, i){
-  element.addEventListener('click', function(event){
-    console.log('i= ' + i);
-    playTurn( element, i);
-  }, false);
-}
-
-function playTurn(element, i){
+function playTurn(){
   //is the game over then no more turns allowed
-  if ( isGameOver ) return;
-  console.log("playTurn at " + i);
-  
-  if ((element.className === "square playerX") || (element.className === "square playerO")) {
+  console.log('i= ' + $(this).attr('id'));
+  if ( isGameOver ) {return};
+
+  console.log("playTurn at " + $(this).attr('id'));  
+
+  if (($(this).attr('class') === "square playerX") || ($(this).attr('class') === "square playerO")) {
     console.log("Square already played");
     alert("Square already played! Try another square");
     return;
   }
   
   if ( player1sTurn=== true ) {
-    element.className += " playerX";
-    element.innerHTML = "X";
+    $(this).addClass("playerX");
+    $(this).html("X");
     player1sTurn = false;
     turns ++;
-    document.getElementById("turn").innerHTML = "It is Player O's Turn";
+    $("#turn").html("It is Player O's Turn");
     } 
 
-    else {
-    element.className += " playerO";
-    element.innerHTML = "O";
+  else {
+    $(this).addClass("playerO");
+    $(this).html("O");
     player1sTurn = true;
     turns ++;
-    document.getElementById("turn").innerHTML = "It is Player X's Turn";
+    $("#turn").html("It is Player X's Turn");
     }
   checkGameOver();
 }
@@ -78,24 +66,24 @@ function checkGameOver(){
 }
 
 function checkLine(a,b,c){
-  if ( elements[a].className ==="square playerX" && ( elements[a].className === elements[b].className ) && ( elements[a].className === elements[c].className ) ){
+  if ( $('.square')[a].className ==="square playerX" && ( $('.square')[a].className === $('.square')[b].className ) && ( $('.square')[a].className === $('.square')[c].className ) ){
     return "X";} 
 
-  else if (elements[a].className ==="square playerO" && ( elements[a].className === elements[b].className ) && ( elements[a].className === elements[c].className )){
+  else if ($('.square')[a].className ==="square playerO" && ( $('.square')[a].className === $('.square')[b].className ) && ( $('.square')[a].className === $('.square')[c].className )){
   return "O";}
   
   else {return 0;}
 }
 
 function resetGame(){
-  for ( var i = 0; i < elements.length; i++ ){
-    elements[i].className = "square";
-    elements[i].innerHTML = "";
+    $('.square').removeClass("playerX");
+    $('.square').removeClass("playerO");
+    $('.square').html("");
     player1sTurn = true;
     isGameOver = false;
-    turns = 0;
-  }
+    turns = 0;  
   console.log("board cleared");
+}
 }
 
 window.addEventListener('load', ready, false);
